7f8229aacbe395def067cf6da38dfd76d0994a11
Jakub47 <jakub7447@gmail.com>
2018-04-15 21:31:46 +0200

    First file -> Add An Abstract Class or another one to allow doing some operations on Zadania objects

---
+++ Exercies(Remind)/Exercies(Remind)/Zadanie.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercies_Remind_
+{
+    class Zadanie : IDane
+    {
+        public string Tresc { get; set; }
+        public string Wyswietl { get; set; }
+        public List<float> Liczby { get; set; }
+
+        public Zadanie(string tresc,string wyswietl)
+        {
+            Tresc = tresc;
+            Wyswietl = wyswietl;
+        }
+
+        public void NapiszTresc()
+        {
+            Console.WriteLine(this.Tresc);
+        }
+        
+        public void PoprosODane()
+        {
+            Console.WriteLine(this.Wyswietl);
+        }
+
+        public void WprowadzLiczby(List<float> liczby)
+        {
+            Liczby = liczby;
+        }
+
+        public void Oblicz()
+        {
+            
+        }
+    }
+}
---
+++ Exercies(Remind)/Exercies(Remind)/ZbiorZadan.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercies_Remind_
+{
+    class ZbiorZadan
+    {
+        public List<Zadanie> Zadania { get; set; }
+
+
+        public ZbiorZadan()
+        {
+            Zadania = new List<Zadanie>()
+            {
+                new Zadanie("Program przeliczający temperaturę w stopniach Celsjusza na temperaturę w stopniach Fahrenheita","Podaj temperature w stopniach Celsjusza"),
+                new Zadanie("Program, który oblicza deltę dla równania kwadratowego ax2 +bx+c=0","Podaj współczynniki a, b oraz c.(Odzielone spacja)"),
+                new Zadanie("Program, który oblicza wskaźnik masy ciała BMI.","Podanj wage w kilogramach oraz wzrost w metrach (Odzielone spacja)")
+            };
+        }
+
+        public void PobierzDaneIObliczwynik(int numerZadania)
+        {
+            var zadanie = Zadania.ElementAt(numerZadania);
+            zadanie.NapiszTresc();
+            zadanie.PoprosODane();
+            var elements = Console.ReadLine().Split(' ').ToList();
+            zadanie.WprowadzLiczby(elements.Select(float.Parse).ToList());
+
+        }
+    }
+}
