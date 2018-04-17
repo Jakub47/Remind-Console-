71a04fdf4284307b3502f8e828acd8bf6bedad3b
Jakub47 <jakub7447@gmail.com>
2018-04-17 11:07:37 +0200

    Java email App Completed -> remind of properties /get;set;/

--- JavaProjects/src/emailapp/Email.java
+++ JavaProjects/src/emailapp/Email.java
@@ -8,7 +8,7 @@ public class Email {
 	private String password;
 	private String department;
 	private String email;
-	private int mailboxCapacity;
+	private int mailboxCapacity = 500;
 	private int defaultpasswordLength = 10;
 	private String alternateEmail;
 	private String companySuffix = "company.com";
@@ -18,11 +18,9 @@ public class Email {
 	{
 		this.firstName = firstName;
 		this.lastName = lastName;
-		System.out.println("EMAIL CREATED" + this.firstName + " " + this.lastName);
 		
 		//Calling a method asking for the department - return the department
 		this.department = setDepartment();
-		System.out.println("Department: " + this.department);
 		
 		//Call a method  that returns a random password
 		this.password = randomPassword(defaultpasswordLength);
@@ -30,12 +28,11 @@ public class Email {
 		
 		//Combine elements to generate an email
 		email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + department + "." + companySuffix;
-		System.out.println("Your email is: " + email);
 	}
 	//Ask for the department 
 	private String setDepartment()
 	{
-		System.out.println("DEPARTMENT CODES\n1 for Sales\n2) for development\n3) for Accounting\n0 for none\nEnter the department code: ");
+		System.out.println("New worker: " + firstName + ".Department Codes:\n1 for Sales\n2) for development\n3) for Accounting\n0 for none\nEnter the department code: ");
 		Scanner in = new Scanner(System.in);
 		int depChoice = in.nextInt();
 		if (depChoice == 1) return "sales";
@@ -57,10 +54,30 @@ public class Email {
 		return new String(password);
 	}
 	//Set the mailbox capacity
-	
+	public void setMailboxCapacity(int capacity)
+	{
+		this.mailboxCapacity = capacity;
+	}
 	//Set the alternate email
-	
+	public void setAlternateEmail(String altEmail)
+	{
+		this.alternateEmail = altEmail;
+	}
 	//Change the password
+	public void changePassword(String password)
+	{
+		this.password = password;
+	}
 	
+	//Encapuslation {return var through methods in C# you can simply use automatic "wlasciwosci"}
+	public int getMailboxCapacity() {return mailboxCapacity; }
+	public String getAlternateEmail() {return alternateEmail; }
+	public String getPassword() {return password; }
 	
+	public String showInfo() 
+	{
+		return "DISPLAY NAME: " + firstName + " " + lastName +
+				"\nCOMPANY EMAIL: " + email + 
+				"\nMAILBOX CAPACITY: " + mailboxCapacity + "mb";
+	}
 }
